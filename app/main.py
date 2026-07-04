c084e839f1bb86e1b24690b54b182218fdfcd871
julianocorso14 <117122040+julianocorso14@users.noreply.github.com>
2026-07-04 14:49:16 +0000

    inicio pipeline 1

--- app/main.py
+++ app/main.py
@@ -70,7 +70,7 @@ def atualizar_tarefa(id: int, tarefa: Tarefa):
     LISTA_TAREFAS[id]["descricao"] = tarefa.descricao
     LISTA_TAREFAS[id]["concluido"] = tarefa.concluido
     if tarefa.concluido == True:
-        requests.post(f"http://localhost:8002/notificar?titulo{tarefa.titulo}&data{datetime.now()}" timeout=100)
+        requests.post(f"http://localhost:8002/notificar?titulo{tarefa.titulo}&data{datetime.now()}", timeout=100)
 
     return mensagem_padrao
 
