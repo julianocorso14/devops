4a18deb509b9a9edaacca62dc2b8399781bdad1a
julianocorso14 <julianocorso@gmail.com>
2026-07-18 13:00:48 -0300

    Metricas 5

--- app/notificacao.py
+++ app/notificacao.py
@@ -2,20 +2,6 @@ from fastapi import FastAPI
 from datetime import datetime
 import logging
 
-LOGGER = logging.getLogger("devops")
-LOGGER.setLevel(logging.INFO)
-
-stream_handler = logging.StreamHandler()
-file_handler   = logging.FileHandler(f"{LOGGER.name}.log", encoding='utf-8')
-formatador     = logging.Formatter(fmt="%(name)s | %(asctime)s | %(levelname)s | %(filename)s:%(lineno)s | %(message)s")
-
-stream_handler.setFormatter(formatador)
-file_handler.setFormatter(formatador)
-
-LOGGER.addHandler(stream_handler)
-LOGGER.addHandler(file_handler)
-
-
 APP_NOTIFICACAO = FastAPI()
 
 # Criar uma rota para receber tarefa finalizada
